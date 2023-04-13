e2c3e353102a6c8cf9c4b244da1716365c890422
PedroAraujooj <pedro.a.vieira21@gmail.com>
2023-04-13 00:04:30 -0300

    new files

--- src/main/java/med/vol/api/controller/MedicosController.java
+++ src/main/java/med/vol/api/controller/MedicosController.java
@@ -1,14 +1,15 @@
 package med.vol.api.controller;
 
-import med.vol.api.medico.DadosCadastroMedico;
-import med.vol.api.medico.Medico;
-import med.vol.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
+import med.vol.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -18,8 +19,29 @@ public class MedicosController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
         System.out.println(dados);
     }
+    @GetMapping
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pagina){
+        return repository.findAllByAtivoTrue(pagina).map(medico -> new DadosListagemMedicos(medico));
+    }
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        Medico medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+   /* @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }*/
+   @DeleteMapping("/{id}")
+   @Transactional
+   public void excluir(@PathVariable Long id) {
+       var medico = repository.getReferenceById(id);
+       medico.excluir();
+   }
 }
--- src/main/java/med/vol/api/medico/Medico.java
+++ src/main/java/med/vol/api/medico/Medico.java
@@ -15,10 +15,13 @@ import med.vol.api.endereco.Endereco;
 @EqualsAndHashCode(of = "id")
 public class Medico {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private  boolean ativo;
     private String nome;
+    private String telefone;
     private String email;
     private String crm;
 
@@ -29,10 +32,29 @@ public class Medico {
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.crm = dados.crm();
+        this.telefone = dados.telefone();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
