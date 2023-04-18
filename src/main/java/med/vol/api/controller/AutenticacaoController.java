a4de8707b1d89542c6e1f2d3938b46341d535abc
Pedro Vieira <pedro.vieira@abramus.org.br>
2023-04-18 15:58:18 -0300

    avanço de codigo

--- src/main/java/med/vol/api/controller/AutenticacaoController.java
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -1,4 +1,9 @@
 package med.vol.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("login")
 public class AutenticacaoController {
 }
