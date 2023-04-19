c4948916fdbf9de137db1eb1a42eb440c0960115
Pedro Vieira <pedro.vieira@abramus.org.br>
2023-04-19 15:23:21 -0300

    avanço curso
    avanço curso

--- src/main/java/med/vol/api/controller/AutenticacaoController.java
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -1,15 +1,15 @@
 package med.vol.api.controller;
 
 import jakarta.validation.Valid;
-import lombok.Value;
+import med.vol.api.domain.usuario.Usuario;
+import med.vol.api.infra.security.DadosTokenJWT;
+import med.vol.api.infra.security.TokenService;
 import med.vol.api.usuario.DadosAutenticacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.token.Token;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAutenticacao){
-        Token token = (Token) new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
+        var token = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
         Authentication authentication = manager.authenticate((Authentication) token);
 
-        return ResponseEntity.ok().build();
+        String tokenReturn = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenReturn));
 
     }
 }
